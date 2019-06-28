ce04036219148ae8a604bf20d80f661fd9964993
WRODRIGU <wrodrigu@proteccion.com.co>
2019-06-27 22:49:43 -0500

    Initial commit

---
+++ src/test/java/co/edu/poli/blblio/poli/PoliApplicationTests.java
@@ -0,0 +1,16 @@
+package co.edu.poli.blblio.poli;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PoliApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
